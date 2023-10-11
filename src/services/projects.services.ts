6cf6d76e6ed2fd9d411efbcb1ae7e73de974f77f
gusmfs <oigostavo@gmail.com>
2023-10-11 14:51:54 -0300

    valeu

--- src/services/projects.services.ts
+++ src/services/projects.services.ts
@@ -28,7 +28,6 @@ export const getProjectService = async(projectId : string) => {
     const queryResult : ProjectResult = await client.query(query, [projectId])
     return queryResult.rows[0] as ProjectObj
 }
-
 export const updateProjectService = async(projectId: number, data: ProjectUpdate) => {
     const queryFormat : string = format(`UPDATE projects SET (%I) = ROW (%L) WHERE "id" = $1 RETURNING *;`,
     Object.keys(data),
